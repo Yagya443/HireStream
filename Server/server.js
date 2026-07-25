eaf4c677dde5d860f37b4eec0eacf69de6073e2f
Yagya Vyas <yagnaakv99@gmail.com>
2026-07-25 23:40:45 +0530

    UserModel and Mongoose connection

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,17 @@
 require('dotenv').config()
 const express = require("express");
+const mongoose=require('mongoose')
+const connectDB = require('./src/libs/dbConnection');
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     return res.send("Hello");
 });
+
+connectDB()
 
 app.listen(process.env.PORT, (req, res) => {
     console.log(`Listening on server ${process.env.PORT}`);
