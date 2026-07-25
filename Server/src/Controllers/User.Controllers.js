320469f6ee5739a972bb8e999031003d70019eaf
Yagya Vyas <yagnaakv99@gmail.com>
2026-07-25 23:57:43 +0530

    Login and signup  controllers

---
+++ Server/src/Controllers/User.Controllers.js
@@ -0,0 +1,77 @@
+const jwt = require("jsonwebtoken");
+const UserModel = require("../Model/User.Model");
+
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const existUser = await email.findOne({ email });
+
+        if (!email) {
+            return res.status(404).json({ message: "Email is required" });
+        }
+        if (!password) {
+            return res.status(404).json({ message: "Password is required" });
+        }
+
+        if (existUser.password != password) {
+            return res.status(400).json({ message: "Incorrect Password" });
+        }
+
+        const token = jwt.sign(
+            {
+                _id: existUser.id,
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "7d",
+            },
+        );
+
+        res.status(200).json({
+            message: "Login successful",
+            token,
+            user: existUser,
+        });
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+const signup = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const existUser = await email.findOne({ email });
+
+        if (existUser) {
+            return res.status(400).json({ message: "User Already exist" });
+        }
+
+        const user = new UserModel({
+            name,
+            email,
+            password,
+        });
+
+        await user.save();
+
+        const token = jwt.sign(
+            {
+                _id: user.id,
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "7d",
+            },
+        );
+
+        res.status(201).json({
+            message: "Account created successfully",
+            token,
+            user,
+        });
+        
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
